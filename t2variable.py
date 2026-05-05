93a6659fe1c2becb7ab26d252e6134148f867712
Honey249882 <honeybgp123@gmail.com>
2026-05-05 16:17:44 +0530

    Add files via upload

---
+++ t2variable.py
@@ -0,0 +1,34 @@
+first_name = 'PRAKASH'
+last_name = 'SENAPATI'
+country = 'HYD'
+city = 'TELENGANA'
+age = 40087
+is_married = True
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+
+person_info = {
+    'firstname': 'Asabeneh',
+    'lastname': 'Yetayeh',
+    'country': 'Finland',
+    'city': 'Helsinki'
+}
+
+print(first_name)
+print(len(first_name))
+print(last_name)
+print(len(last_name))
+print(country)
+print(city)
+print(age)
+print(is_married)
+print(skills)
+print(person_info)
+
+first_name, last_name, country, age, is_married = 'prakash', 'senapati', 'Helsink', 250, True
+
+print(first_name, last_name, country, age, is_married)
+print(first_name)
+print(last_name)
+print(country)
+print(age)
+print(is_married)
---
+++ t4variables.py
@@ -0,0 +1,63 @@
+import sys
+print(sys.version)
+
+
+print(3 + 5)
+print(10 - 5)
+print(10 + 5)
+print(10 / 5)
+print(10 // 5)
+print(10 * 5)
+
+
+
+
+print(3 * 2)
+print(3 ** 2)
+print(10 % 5)
+
+print(3 * 2 + 3)
+print(3 * 2 + 3 - 2)
+
+print(10 * 3 - 2 + 1)
+print(10 * 3 - (2 + 1))
+
+print(5 + 1)
+
+print(2 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+
+
+
+print("apple")
+print('apple')
+
+print("apple\nfruit\nis\ngood for health")
+
+v = 5
+print(v)
+print(id(v))
+
+var = 10
+print(var)
+
+nit = 25
+print(nit)
+
+nit1 = 30
+print(nit1)
+
+var_ = 6
+print(var_)
+
+
+
+
+import keyword
+print(keyword.kwlist)
+
+
+For = 3
+print(For)
